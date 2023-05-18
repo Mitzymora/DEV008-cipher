d8d2980058bde8b045d33c020e17bddd790a026d
Mitzy Mora <mitzamora30@gmail.com>
2023-05-18 18:30:31 -0500

    cipher Mitzy

--- src/index.js
+++ src/index.js
@@ -1,46 +1,39 @@
-import cipher from './cipher.js';
-console.log(cipher);{
-};
+import cipher from "./cipher.js";
 
 window.addEventListener("load", inicio, true);
 //Mayusculas
 function inicio() {
-    document.getElementById("textoparacifrar").addEventListener("keyup",function(){
-        this.value = this.value.toUpperCase();    
-    }, true);
-    //Funcion para cifrar
-    document.getElementById("cifrar").addEventListener("click", function(){
-        let texto = document.getElementById("textoparacifrar").value;
-        let movimiento = document.getElementById("cantidad").value;
-        document.getElementById("textocifrado").value = cifrar (texto, movimiento);
-    }, true);
+  document.getElementById("textoparacifrar").addEventListener(
+    "keyup",
+    function () {
+      this.value = this.value.toUpperCase();
+    },
+    true
+  );
 
-    //Funcion para descifrar
-    document.getElementById("descifrar").addEventListener("click",function(){
-        let texto = document.getElementById("textoparacifrar").value;
-        let movimiento = document.getElementById("cantidad").value;
-        document.getElementById("textocifrado").value = descifrar (texto, movimiento);
-    }, true);
+  document.getElementById("botoncifrar").addEventListener(
+    "click",
+    function () {
+      const texto = document.getElementById("textoparacifrar").value;
+      const movimiento = document.getElementById("cantidad").value;
+      document.getElementById("textocifrado").value = cipher.encode(
+        movimiento,
+        texto
+      );
+    },
+    true
+  );
+
+  document.getElementById("botondescifrar").addEventListener(
+    "click",
+    function () {
+      const texto = document.getElementById("textoparacifrar").value;
+      const movimiento = document.getElementById("cantidad").value;
+      document.getElementById("textocifrado").value = cipher.decode(
+        movimiento,
+        texto
+      );
+    },
+    true
+  );
 }
-
-function cifrar(texto, movimiento){
-    let resultado ="";
-    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    movimiento = (movimiento %26 + 26) %26;
-
-    if (texto){
-        for (let i = 0; i<texto.length; i++){
-            if(letras.indexOf(texto[i])!=-1) {
-                let posicion = ((letras.indexOf(texto[i])+movimiento)%26);
-                resultado += letras [posicion];
-            }
-            else
-            resultado += texto[i];
-        }
-
-    }
-    return resultado;
-
-}
-
